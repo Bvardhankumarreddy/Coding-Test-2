6908d54e2af4efcbe1b77b95ce34fee439844741
Bvardhankumarreddy <62837575+Bvardhankumarreddy@users.noreply.github.com>
2023-02-02 11:16:15 +0530

    Coding Test-2

---
+++ src/app/IUser.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+    emp_code: string;
+    name: String;
+    designation: String;
+    email: string;
+    loc : string
+    
+}
---
+++ test.sql
@@ -0,0 +1,15 @@
+Create Table emp_det (
+
+	Emp_code varchar(20),
+	name varchar(30),
+	designation varchar(20),
+	email varchar(30),
+	loc varchar(20)
+)
+
+select * from emp_det;
+
+
+Insert Into emp_det values ('1', 'Vardhan','Manager','bajaj1@gmail.com','Pune'),
+('2','Hema','SDE','bajaj2@gmail.com','Pune'),
+('3', 'Virat','Project Lead','bajaj3@gmail.com','Bangalore')
